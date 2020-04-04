caa79300d7a5dc476e3fbf0d1c7385d0acdfa076
konstantin.anosov <konstantin.anosov@eltex.loc>
2020-04-04 18:04:05 +0700

    change to gofast

--- entrypoint.bash
+++ entrypoint.bash
@@ -7,9 +7,10 @@ function generate_types_golang {
     for x in $(find -name *.proto)
     do 
         echo "Start generate $x contract for golang models..."
-        exec $(protoc-wrapper -I/usr/include --gogofast_out=./ -I. $x)
+        exec $(protoc-wrapper -I/usr/include --gofast_out=./ -I. $x)
         if [[ $? -eq 0 ]]; then 
             echo "Completely generate $x"
+            cat $x
         else
             echo "Generated with problems."
             echo "Stopping process..."
