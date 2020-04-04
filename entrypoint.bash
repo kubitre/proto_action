c9f4230cc537f04a328b2cf748363ad1e54e9bb0
konstantin.anosov <konstantin.anosov@eltex.loc>
2020-04-04 18:18:50 +0700

    upgrade

--- entrypoint.bash
+++ entrypoint.bash
@@ -7,10 +7,9 @@ function generate_types_golang {
     for x in $(find -name *.proto)
     do 
         echo "Start generate $x contract for golang models..."
-        exec $(protoc-wrapper -I/usr/include --gofast_out=./ -I. $x)
+        exec $(protoc-wrapper -I/usr/include --gogofast_out=./ -I. $x)
         if [[ $? -eq 0 ]]; then 
             echo "Completely generate $x"
-            cat $x
         else
             echo "Generated with problems."
             echo "Stopping process..."
